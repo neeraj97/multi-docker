3c0d50e71f05266bfbb485e4b4db25bc9cee12f3
neeraj97 <c.neerajd007@gmail.com>
2020-05-07 18:32:34 +0530

    edited Dockerrun.aws.json

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Fib Calculator</h3>
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page</Link>
       </header>
